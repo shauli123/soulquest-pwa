58236f19cb1b8aad26b0a87255abcdb08504ed22
Shauli Sultan <sultansholi35@gmail.com>
2026-05-26 13:40:54 +0300

    Step 2: Configure Next.js (next.config.ts, tsconfig.json)

---
+++ next.config.ts
@@ -0,0 +1,15 @@
+import withPWAInit from "@ducanh2912/next-pwa";
+import type { NextConfig } from "next";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  skipWaiting: true,
+});
+
+const nextConfig: NextConfig = {
+  /* config options here */
+};
+
+export default withPWA(nextConfig);
